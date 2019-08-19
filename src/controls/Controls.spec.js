63c1da2625239a969f5854556f10b2a4571ec9a2
Cory Hedeen <hedeencory@gmail.com>
2019-08-19 18:09:37 -0400

    Build snapshot

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,19 @@
 // Test away!
+import React from 'react';
+import Controls from './Controls';
+import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+
+describe('<Controls />', () => {
+  it('looks great', () => {
+    const tree = renderer.create(<Controls />);
+    expect(tree.toJSON()).toMatchSnapshot()
+  })
+  it('locks', () => {
+    const mock = jest.fn();
+
+    const { getByText } = render(<Controls toggleLocked={mock}/>)
+
+    const button = getByText(/unlock gate/i);
+  })
+})
